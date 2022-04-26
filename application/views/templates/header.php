23d6e51b78a70f9dfb3046672d7953d88753eb6e
alfathmuqoddas <alfath.muqoddas@gmail.com>
2022-04-27 01:19:18 +0700

    update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,10 +14,10 @@
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="<?php echo base_url(); ?>">Project Management System</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarHeader">
           <ul class="navbar-nav">
           <?php if(!$this->ion_auth->logged_in()): ?>
             <li class="nav-item"><a class="nav-link btn btn-primary btn-sm mr-2" href="<?php echo base_url(); ?>auth">Login</a></li>
@@ -27,6 +27,7 @@
             <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>manager">Manager Input</a></li>
             <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>developer">Developer Input</a></li>
             <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>amgr">Amgr Input</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>auth">Users Setting</a></li>
           <?php elseif($this->ion_auth->in_group('user_biasa')): ?>
             <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>input">User Input</a></li>
           <?php elseif($this->ion_auth->in_group('manager')): ?>
@@ -39,9 +40,7 @@
           <?php $user = $this->ion_auth->user()->row(); if($user) : ?>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <?php
-                echo $user->email;
-                ?>
+                <?php echo $user->email; ?>
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="<?php echo base_url(); ?>users/logout">Logout</a>
